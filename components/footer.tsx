9e44580ab794bc12fcab53147acec2e84d8ee71d
Knowyoursales <oldbassha@gmail.com>
2024-09-26 08:33:11 +0300

    fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { Facebook, Twitter, Instagram, Linkedin, Github } from 'lucide-react'
+import {  Linkedin } from 'lucide-react'
 
 export default function Footer() {
   return (
